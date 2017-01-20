3d876d5c7b0bb58ccb8db01d46acc32bdda459c4
Daisuke Baba <baba.daisuke@gmail.com>
2017-01-20 11:00:17 +0900

    Refactor precondition checking

--- backup.sh
+++ backup.sh
@@ -8,31 +8,7 @@
 # Bailout if any command fails
 set -e
 
-case $STORAGE_TYPE in
-	s3)
-		if [ -z "$ACCESS_KEY" ] || [ -z "$SECRET_KEY" ] || [ -z "$BUCKET" ]; then
-			echo "[$STORAGE_TYPE] Cannot access to s3 with the given information"
-			exit 1
-		fi
-		;;
-  swift)
-		if [ -z "$OS_TENANT_NAME" ] || [ -z "$OS_USERNAME" ] || [ -z "$OS_PASSWORD" ] || [ -z "$CONTAINER" ] || [ -z "$OS_AUTH_URL" ]; then
-			echo "[$STORAGE_TYPE] Cannot access to swift with the given information"
-			exit 1
-		fi
-		;;
-	local)
-		if [ ! -d "$BACKUP_DIR" ]; then
-			echo "[$STORAGE_TYPE] Cannot backup to the missing directory"
-			exit 1
-		fi
-		;;
-	*)
-		echo "Unknown storage type => $STORAGE_TYPE. s3, swift or local is valid."
-		exit 1
-		;;
-esac
-
+. /_validate.sh
 
 # Specify mysql host (mysql by default)
 MYSQL_HOST=${MYSQL_HOST:-mysql}
