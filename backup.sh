9f18e06d6a9cc49369922e7dea168c2f71928531
Daisuke Baba <baba.daisuke@gmail.com>
2017-01-20 18:15:33 +0900

    Upload one by one in order to avoid unknown auth error in a Swift server

--- backup.sh
+++ backup.sh
@@ -42,7 +42,11 @@ case $STORAGE_TYPE in
   swift)
 		# Upload the backups to Swift
 		cd $DIR
-		swift upload $CONTAINER .
+		for f in `ls *.sql.gz`
+		do
+			# Avoid Authorization Failure error
+			swift upload $CONTAINER ${f}
+		done
 		;;
 	local)
 	  # move the backup files in the temp directory to the backup directory
