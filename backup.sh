397f7a5391a062650470cc7e71f57523477a334e
Daisuke Baba <baba.daisuke@gmail.com>
2017-03-02 11:02:18 +0900

    Fix indentation

--- backup.sh
+++ backup.sh
@@ -24,34 +24,34 @@ TS=$(date +%Y-%m-%d-%H%M%S)
 # Backup all databases, unless a list of databases has been specified
 if [ -z "$DBS" ]
 then
-	# Backup all DB's in bulk
-	mysqldump -uroot -p$MYSQL_ROOT_PASSWORD -h$MYSQL_HOST --add-drop-database --all-databases $MYSQLDUMP_OPTIONS | gzip > $DIR/${PREFIX}all-databases-$TS.sql.gz
+  # Backup all DB's in bulk
+  mysqldump -uroot -p$MYSQL_ROOT_PASSWORD -h$MYSQL_HOST --add-drop-database --all-databases $MYSQLDUMP_OPTIONS | gzip > $DIR/${PREFIX}all-databases-$TS.sql.gz
 else
-	# Backup each DB separately
-	for DB in $DBS
-	do
-		mysqldump -uroot -p$MYSQL_ROOT_PASSWORD -h$MYSQL_HOST --add-drop-database -B $DB $MYSQLDUMP_OPTIONS | gzip > $DIR/$PREFIX$DB-$TS.sql.gz
-	done
+  # Backup each DB separately
+  for DB in $DBS
+  do
+    mysqldump -uroot -p$MYSQL_ROOT_PASSWORD -h$MYSQL_HOST --add-drop-database -B $DB $MYSQLDUMP_OPTIONS | gzip > $DIR/$PREFIX$DB-$TS.sql.gz
+  done
 fi
 
 case $STORAGE_TYPE in
-	s3)
-		# Upload the backups to S3 --region=$REGION
-		s3cmd --access_key=$ACCESS_KEY --secret_key=$SECRET_KEY --region=$REGION sync $DIR/ $BUCKET
-		;;
+  s3)
+    # Upload the backups to S3 --region=$REGION
+    s3cmd --access_key=$ACCESS_KEY --secret_key=$SECRET_KEY --region=$REGION sync $DIR/ $BUCKET
+    ;;
   swift)
-		# Upload the backups to Swift
-		cd $DIR
-		for f in `ls *.sql.gz`
-		do
-			# Avoid Authorization Failure error
-			swift upload $CONTAINER ${f}
-		done
-		;;
-	local)
-	  # move the backup files in the temp directory to the backup directory
-		mv $DIR/* $BACKUP_DIR/
-		;;
+    # Upload the backups to Swift
+    cd $DIR
+    for f in `ls *.sql.gz`
+    do
+      # Avoid Authorization Failure error
+      swift upload $CONTAINER ${f}
+    done
+    ;;
+  local)
+    # move the backup files in the temp directory to the backup directory
+    mv $DIR/* $BACKUP_DIR/
+    ;;
 esac
 
 # Clean up
--- restore.sh
+++ restore.sh
@@ -13,54 +13,54 @@ set -e
 # Check that a backup is specified or list all backups!
 if [ -z "$1" ]
 then
-	case $STORAGE_TYPE in
-		s3)
-			BUCKET_PREFIX=$BUCKET
+  case $STORAGE_TYPE in
+    s3)
+      BUCKET_PREFIX=$BUCKET
       if [ -n "$PREFIX" ]; then
         BUCKET_PREFIX=$BUCKET/$PREFIX
       fi
       s3cmd --access_key=$ACCESS_KEY --secret_key=$SECRET_KEY --region=$REGION ls $BUCKET_PREFIX | grep .sql.gz
-			;;
-	  swift)
-			FILE_PREFIX=""
-			if [ -n "$PREFIX" ]; then
-				FILE_PREFIX="--prefix ${PREFIX}"
-			fi
-			swift list $CONTAINER $FILE_PREFIX | grep .sql.gz
-			;;
-		local)
-			if [ -n "$PREFIX" ]; then
+      ;;
+    swift)
+      FILE_PREFIX=""
+      if [ -n "$PREFIX" ]; then
+        FILE_PREFIX="--prefix ${PREFIX}"
+      fi
+      swift list $CONTAINER $FILE_PREFIX | grep .sql.gz
+      ;;
+    local)
+      if [ -n "$PREFIX" ]; then
         ls ${BACKUP_DIR}/*.sql.gz | xargs -n 1 basename | grep $PREFIX
       else
         ls ${BACKUP_DIR}/*.sql.gz | xargs -n 1 basename
       fi
-			;;
-	esac
+      ;;
+  esac
 
 else
-	# Create a temporary directory to hold the backup files
-	DIR=$(mktemp -d)
+  # Create a temporary directory to hold the backup files
+  DIR=$(mktemp -d)
 
-	case $STORAGE_TYPE in
-		s3)
-			# Get the backups from S3
-			s3cmd --access_key=$ACCESS_KEY --secret_key=$SECRET_KEY --region=$REGION get $BUCKET$1 $DIR/$1
-			;;
-	  swift)
-			swift download $CONTAINER $1 --output $DIR/$1
-			;;
-		local)
-			cp -f $BACKUP_DIR/$1 $DIR/$1
-			;;
-	esac
+  case $STORAGE_TYPE in
+    s3)
+      # Get the backups from S3
+      s3cmd --access_key=$ACCESS_KEY --secret_key=$SECRET_KEY --region=$REGION get $BUCKET$1 $DIR/$1
+      ;;
+    swift)
+      swift download $CONTAINER $1 --output $DIR/$1
+      ;;
+    local)
+      cp -f $BACKUP_DIR/$1 $DIR/$1
+      ;;
+  esac
 
-	# Specify mysql host (mysql by default)
-	MYSQL_HOST=${MYSQL_HOST:-mysql}
-	MYSQL_ROOT_PASSWORD=${MYSQL_ENV_MYSQL_ROOT_PASSWORD:-${MYSQL_ROOT_PASSWORD}}
+  # Specify mysql host (mysql by default)
+  MYSQL_HOST=${MYSQL_HOST:-mysql}
+  MYSQL_ROOT_PASSWORD=${MYSQL_ENV_MYSQL_ROOT_PASSWORD:-${MYSQL_ROOT_PASSWORD}}
 
-	# Restore the DB
-	gunzip < $DIR/$1 | mysql -uroot -p$MYSQL_ROOT_PASSWORD -h$MYSQL_HOST
+  # Restore the DB
+  gunzip < $DIR/$1 | mysql -uroot -p$MYSQL_ROOT_PASSWORD -h$MYSQL_HOST
 
-	# Clean up
-	rm -rf $DIR
+  # Clean up
+  rm -rf $DIR
 fi
