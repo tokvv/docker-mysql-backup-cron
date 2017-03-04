9d6237231173cc3ff70237c37abe11235a9f04c7
Daisuke Baba <baba.daisuke@gmail.com>
2017-03-04 19:49:09 +0900

    Add a new feature to restore from the latest backup file

--- restore.sh
+++ restore.sh
@@ -9,37 +9,9 @@
 set -e
 
 . /_validate.sh
+. /_list.sh
 
-# Check that a backup is specified or list all backups!
-if [ -z "$1" ]
-then
-  case $STORAGE_TYPE in
-    s3)
-      BUCKET_PREFIX=$BUCKET
-      if [ -n "$PREFIX" ]; then
-        BUCKET_PREFIX=$BUCKET/$PREFIX
-      fi
-      s3cmd --access_key=$ACCESS_KEY --secret_key=$SECRET_KEY --region=$REGION ls $BUCKET_PREFIX | grep .sql.gz
-      ;;
-    swift)
-      FILE_PREFIX=""
-      if [ -n "$PREFIX" ]; then
-        FILE_PREFIX="--prefix ${PREFIX}"
-      fi
-      swift list $CONTAINER $FILE_PREFIX | grep .sql.gz
-      ;;
-    local)
-      cd ${BACKUP_DIR}
-      find . -type f -name "*.sql.gz" | grep "^./${PREFIX}"
-      ;;
-  esac
-
-else
-  # Create a temporary directory to hold the backup files
-  find /tmp -type d | grep -v "^/tmp$" | xargs rm -fr
-  DIR=$(mktemp -d)
-  FILE_NAME=`basename $1`
-
+function restore {
   case $STORAGE_TYPE in
     s3)
       # Get the backups from S3
@@ -52,6 +24,30 @@ else
       cp -f $BACKUP_DIR/$1 $DIR
       ;;
   esac
+}
+
+# Check that a backup is specified or list all backups!
+if [ -z "$1" ]
+then
+  list_backup_files
+  for f in ${ALL_BACKUP_FILES}
+  do
+    echo $f
+  done
+else
+  # Create a temporary directory to hold the backup files
+  find /tmp -type d | grep -v "^/tmp$" | xargs rm -fr
+  DIR=$(mktemp -d)
+  BACKUP_FILE_PATH=$1
+
+  if [ "${BACKUP_FILE_PATH}" == "__latest__" ]; then
+    list_backup_files
+    BACKUP_FILE_PATH=${LATEST_BACKUP}
+  fi
+
+  FILE_NAME=`basename ${BACKUP_FILE_PATH}`
+  restore ${BACKUP_FILE_PATH}
+  echo "${BACKUP_FILE_PATH}"
 
   # Specify mysql host (mysql by default)
   MYSQL_HOST=${MYSQL_HOST:-mysql}
